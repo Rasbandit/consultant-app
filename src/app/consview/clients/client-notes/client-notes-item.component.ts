02134182c266c81a542ca0ad024f9373068ff36c
dougglez <maxfield.douglas@gmail.com>
2016-10-06 15:32:06 -0600

    header and front page fixes

--- src/app/consview/clients/client-notes/client-notes-item.component.ts
+++ src/app/consview/clients/client-notes/client-notes-item.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Note } from './note';
 
-
-
 @Component({
   selector: 'app-client-notes-item',
   templateUrl: './client-notes-item.component.html',
